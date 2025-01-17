022b19bbb830a7df41d21e7291154de7d4bede8d
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-17 13:16:17 -0600

    Proteccion rutas logeo router

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,15 +31,16 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-  auth.onAuthStateChanged(user => {
-    const isAuthenticated = !!user;
-    if (requiresAuth && !isAuthenticated) {
-      next('/');
-    } else {
-      next();
-    }
-  });
+  const isAuthenticated = !!auth.currentUser;
+
+  if (requiresAuth && !isAuthenticated) {
+    next('/');
+  } else {
+    next();
+  }
 });
+
 export default router;
