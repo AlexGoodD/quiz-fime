af1fa6b5a81819b7015a5aa4c8a80ee332b832ae
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-18 21:35:29 -0600

    Formato de preguntas ID/Type/Question/Answer

---
+++ src/assets/questions.ts
@@ -0,0 +1,24 @@
+export const questionsData = [
+  {
+    id: 1,
+    type: 'slider',
+    question: '¿Cómo calificarías tu experiencia?',
+  },
+  {
+    id: 2,
+    type: 'trueFalse',
+    question: '¿Es verdadero o falso?',
+  },
+  {
+    id: 3,
+    type: 'multipleChoice',
+    question: 'Selecciona una opción',
+    options: ['Opción 1', 'Opción 2', 'Opción 3'],
+  },
+  {
+    id: 4,
+    type: 'multipleChoice',
+    question: 'Selecciona una opción',
+    options: ['Opción 1', 'Opción 5', 'Opción 3'],
+  },
+];
