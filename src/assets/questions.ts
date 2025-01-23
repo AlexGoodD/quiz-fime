276616556cecb4f7cef9be05ee374c576947b623
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-23 15:22:11 -0600

    Cambio orden preguntas

--- src/assets/questions.ts
+++ src/assets/questions.ts
@@ -1,20 +1,20 @@
 export const questionsData = [
   {
     id: 1,
+    type: 'multipleChoice',
+    question: 'Al resolver problemas me gusta empezar por:',
+    options: ['Analizar las causas', 'Analizar los problemas', 'Analizar las soluciones'],
+  },
+  {
+    id: 2,
     type: 'slider',
-    question: '¿Cómo calificarías tu experiencia?',
+    question: '¿Del 1 al 5 que tan proactivo eres? (1 es poco proactivo y 5 es muy proactivo)',
     min: 1,
     max: 5,
   },
   {
-    id: 2,
-    type: 'trueFalse',
-    question: '¿Es verdadero o falso?',
-  },
-  {
     id: 3,
-    type: 'multipleChoice',
-    question: 'Selecciona una opción',
-    options: ['Opción 1', 'Opción 2', 'Opción 3'],
-  },
+    type: 'trueFalse',
+    question: '¿Si tuvieras que definirte como una persona hábil en matemáticas?',
+  }
 ];
