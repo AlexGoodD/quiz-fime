e83885f366ae4568665af9c8b1cb73ca77774945
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-23 12:06:11 -0600

    Implementación de ventana de administrador

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,14 +1,32 @@
 <template>
-    <AppHeader />
-    <div>
-      <ResultsQuiz />
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import ResultsQuiz from '../components/ResultsQuiz.vue';
-  import AppHeader from '../components/AppHeader.vue';
-  </script>
-  
-  <style scoped>
-  </style>
+  <AppHeader />
+  <div>
+    <AdminResults v-if="isAdmin" />
+    <ResultsQuiz v-else />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+import { subscribeToUserAuth } from '../services/adminService';
+import ResultsQuiz from '../components/ResultsQuiz.vue';
+import AppHeader from '../components/AppHeader.vue';
+import AdminResults from '../components/adminPage.vue';
+
+const isAdmin = ref(localStorage.getItem('isAdmin') === 'true');
+
+onMounted(() => {
+  const unsubscribe = subscribeToUserAuth((isAdminValue) => {
+    isAdmin.value = isAdminValue;
+  });
+
+  if (unsubscribe) {
+    onUnmounted(() => {
+      unsubscribe();
+    });
+  }
+});
+</script>
+
+<style scoped>
+</style>
