b40becd5fa6612fc4b4d187b605f9e296c5fea3f
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-25 13:59:06 -0600

    Añadiendo posgrados

---
+++ src/assets/posgrados.ts
@@ -0,0 +1,161 @@
+export const posgradoData = [
+    {
+        id: 1,
+        type: 'Maestria',
+        name: 'Maestría en Logística y Cadena de Suministro',
+        areaPoints: {
+            'Logística y Cadena de Suministro': 10,
+            'Producción y Calidad': 5,
+        }
+    },
+    {
+        id: 2,
+        type: 'Maestria',
+        name: 'Maestría en Ciencias de la Ingeniería con Orientación en Materiales'
+    },
+    {
+        id: 3,
+        type: 'Maestria',
+        name: 'Maestría en Ciencias de la Ingeniería Eléctrica'
+    },
+    {
+        id: 4,
+        type: 'Maestria',
+        name: 'Maestría en Ciencias de la Ingeniería con orientación en Nanotecnología'
+    },
+    {
+        id: 5,
+        type: 'Maestria',
+        name: 'Maestría en Ciencias de la Ingeniería con Orientación en Tecnología Energética'
+    },
+    {
+        id: 6,
+        type: 'Maestria',
+        name: 'Maestría en Ciencias de la Ingeniería con orientación en Energías Térmica y Renovable'
+    },
+    {
+        id: 7,
+        type: 'Maestria',
+        name: 'Maestría en Ciencias de la Ingeniería con orientación en Sistemas'
+    },
+    {
+        id: 8,
+        type: 'Maestria',
+        name: 'Maestría en Ciencias de la Ingeniería Automotriz'
+    },
+    {
+        id: 9,
+        type: 'Maestria',
+        name: 'Maestría en Ingeniería Aeronáutica'
+    },
+    {
+        id: 10,
+        type: 'Maestria',
+        name: 'Maestría en Ingeniería con orientación en Tecnologías de la Información'
+    },
+    {
+        id: 11,
+        type: 'Maestria',
+        name: 'Maestría en Ingeniería con orientación en Manufactura'
+    },
+    {
+        id: 12,
+        type: 'Maestria',
+        name: 'Maestría en Ingeniería con orientación en Mecánica'
+    },
+    {
+        id: 13,
+        type: 'Maestria',
+        name: 'Maestría en Ingeniería con orientación en Telecomunicaciones'
+    },
+    {
+        id: 14,
+        type: 'Maestria',
+        name: 'Maestría en Ingeniería con orientación en Mecatrónica'
+    },
+    {
+        id: 15,
+        type: 'Maestria',
+        name: 'Maestría en Ingeniería con orientación en Eléctrica'
+    },
+    {
+        id: 16,
+        type: 'Maestria',
+        name: 'Maestría en Administración de Organizaciones con orientación en Innovación Tecnológica'
+    },
+    {
+        id: 17,
+        type: 'Maestria',
+        name: 'Maestría en Administración de Organizaciones con orientación en Dirección de Procesos Formativos'
+    },
+    {
+        id: 18,
+        type: 'Maestria',
+        name: 'Maestría en Administración de Organizaciones con orientación en Producción y Calidad'
+    },
+    {
+        id: 19,
+        type: 'Maestria',
+        name: 'Maestría en Administración de Organizaciones con orientación en Comercio Internacional'
+    },
+    {
+        id: 20,
+        type: 'Maestria',
+        name: 'Maestría en Administración de Organizaciones con orientación en Relaciones Industriales'
+    },
+    {
+        id: 21,
+        type: 'Maestria',
+        name: 'Maestría en Administración de Organizaciones con orientación en Finanzas'
+    },
+    {
+        id: 22,
+        type: 'Doctorado',
+        name: 'Doctorado en Ingeniería de Materiales'
+    },
+    {
+        id: 23,
+        type: 'Doctorado',
+        name: 'Doctorado en Ingeniería Eléctrica'
+    },
+    {
+        id: 24,
+        type: 'Doctorado',
+        name: 'Doctorado en Energías Térmica y Renovable'
+    },
+    {
+        id: 25,
+        type: 'Doctorado',
+        name: 'Doctorado en Ingeniería de Sistemas'
+    },
+    {
+        id: 26,
+        type: 'Doctorado',
+        name: 'Doctorado en Ciencias de la Ingeniería Aeronáutica'
+    },
+    {
+        id: 27,
+        type: 'Doctorado',
+        name: 'Doctorado en Ingeniería con orientación en Tecnologías de la Información'
+    },
+    {
+        id: 28,
+        type: 'Especialidad',
+        name: 'Especialidad en Ingeniería de Industria Inteligente'
+    },
+    {
+        id: 29,
+        type: 'Especialidad',
+        name: 'Especialidad en Electromovilidad'
+    },
+    {
+        id: 30,
+        type: 'Especialidad',
+        name: 'Especialidad en Logística y Cadena de Suministro'
+    }
+];
+
+/*
+Cada posgrado contiene un identificado, el tipo (Maestría, Doctorado, Especialidad), 
+el nombre, y los puntos asignados por área.
+*/
