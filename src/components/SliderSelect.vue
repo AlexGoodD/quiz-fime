f44b85f335d69e8eff03a4230cf5c9945bf5e8e2
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-26 12:09:36 -0600

    Mejora diseño de slidercomponent

--- src/components/SliderSelect.vue
+++ src/components/SliderSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="slider-container">
     <h1>{{ question }}</h1>
     <input
       type="range"
@@ -7,8 +7,27 @@
       :max="max"
       v-model="sliderValue"
       @input="updateValue"
+      :style="{
+        '--value': sliderValue,
+        '--min': min,
+        '--max': max,
+      }"
+      class="slider"
     />
-    <span>{{ sliderValue }}</span>
+    <div class="tickmarks">
+      <span
+        v-for="n in max - min + 1"
+        :key="n"
+        :data-active="n + min - 1 <= sliderValue"
+        :class="{
+          tickmark: true,
+          'tickmark--first': n === 1,
+          'tickmark--last': n + min - 1 === sliderValue,
+        }"
+      >
+      </span>
+    </div>
+    <div class="slider-value">{{ sliderValue }}</div>
   </div>
 </template>
 
@@ -28,3 +47,77 @@ const emits = defineEmits<{
 
 const { sliderValue, updateValue } = useSliderQuestion(props, emits);
 </script>
+
+<style scoped>
+.slider-container {
+  position: relative;
+  justify-content: center;
+  width: 80%;
+}
+
+.slider {
+  width: 100%;
+  -webkit-appearance: none;
+  appearance: none;
+  height: 10px;
+  background: linear-gradient(
+    to right,
+    #4caf50 0%,
+    #4caf50 calc((var(--value) - var(--min)) / (var(--max) - var(--min)) * 100%),
+    #ddd calc((var(--value) - var(--min)) / (var(--max) - var(--min)) * 100%),
+    #ddd 100%
+  );
+  border-radius: 5px;
+  outline: none;
+  transition: background 0.3s;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  background: #4caf50;
+  cursor: pointer;
+  border-radius: 50%;
+}
+
+.slider::-moz-range-thumb {
+  width: 20px;
+  height: 20px;
+  background: #4caf50;
+  cursor: pointer;
+  border-radius: 50%;
+}
+
+.tickmarks {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 10px;
+  position: relative;
+}
+
+.tickmark {
+  position: relative;
+  width: 2px;
+  height: 10px;
+  background: #ddd;
+  transition: background 0.3s, height 0.5s;
+  height: 10px;
+}
+
+.tickmark[data-active="true"] {
+  background: #4caf50;
+}
+
+.tickmark--first[data-active="true"],
+.tickmark--last[data-active="true"] {
+  height: 20px;
+}
+
+.slider-value {
+  position: relative;
+  top: 10px;
+}
+</style>
