767e84a42f9f587c4d08c3be12f9fb2c6505bb06
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-26 14:30:50 -0600

    Correccion ruta protegida

---
+++ src/views/AdminView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <AdminResults v-if="isAdmin" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+import { subscribeToUserAuth } from "../services/adminService";
+import AdminResults from "../components/adminPage.vue";
+
+const isAdmin = ref(localStorage.getItem("isAdmin") === "true");
+
+onMounted(() => {
+  const unsubscribe = subscribeToUserAuth((isAdminValue) => {
+    isAdmin.value = isAdminValue;
+  });
+
+  if (unsubscribe) {
+    onUnmounted(() => {
+      unsubscribe();
+    });
+  }
+});
+</script>
+
+<style scoped></style>
