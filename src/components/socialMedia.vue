466759399f7e0cb0b78d1aaa5ad984d8798686eb
AlexGoodD <alejandrochavarb@gmail.com>
2025-01-26 14:29:04 -0600

    Icono social eliminado

--- src/components/socialMedia.vue
+++ src/components/socialMedia.vue
@@ -1,115 +1,93 @@
-<style scoped>
-  ul {
-    list-style: none;
-  }
-
-  .example-2 {
-    display: flex;
-    justify-content: center;
-    gap: 5%;
-    align-items: center;
-  }
-  .example-2 .icon-content {
-    margin: 0 10px;
-    position: relative;
-  }
-  .example-2 .icon-content .tooltip {
-    position: absolute;
-    top: -10px;
-    left: 50%;
-    transform: translateX(50%);
-    color: #fff;
-    padding: 6px 10px;
-    border-radius: 15px;
-    opacity: 0;
-    visibility: hidden;
-    font-size: 14px;
-    transition: all 0.3s ease;
-  }
-  .example-2 .icon-content:hover .tooltip {
-    opacity: 1;
-    visibility: visible;
-    top: 6px;
-  }
-  .example-2 .icon-content a {
-    position: relative;
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 50px;
-    height: 50px;
-    border-radius: 20%;
-    color: #4d4d4d;
-    background-color: #ffff;
-    transition: all 0.3s ease-in-out;
-    box-shadow: 3px 2px 45px 0px rgb(0 0 0 / 10%);
-
-  }
-  .example-2 .icon-content a svg {
-    position: relative;
-    z-index: 1;
-    width: 30px;
-    height: 30px;
-  }
-  .example-2 .icon-content a:hover {
-    color: white;
-  }
-  .example-2 .icon-content a .filled {
-    position: absolute;
-    top: auto;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 0;
-    background-color: #000;
-    transition: all 0.3s ease-in-out;
-  }
-  .example-2 .icon-content a:hover .filled {
-    height: 100%;
-  }
-  .example-2 .icon-content a[data-social="twitter"] .filled,
-  .example-2 .icon-content a[data-social="twitter"] ~ .tooltip {
-    background-color: #0099da;
-  }
-  .example-2 .icon-content a[data-social="facebook"] .filled,
-  .example-2 .icon-content a[data-social="facebook"] ~ .tooltip {
-    background-color: #143595;
-  }
-</style>
-
 <template>
   <ul class="example-2">
-    <li class="icon-content">
+    <li class="icon-content" v-for="(icon, index) in icons" :key="index">
       <a
         href="javascript:void(0)"
-        aria-label="Twitter"
-        data-social="twitter"
-        @click="() => shareOnTwitter(index, userAnswerRefs)"
+        :aria-label="icon.ariaLabel"
+        :data-social="icon.dataSocial"
+        @click="icon.clickHandler"
       >
-        <div class="filled"></div>
-        <svg fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="7935ec95c421cee6d86eb22ecd12f847"> <path style="display: inline;" d="M459.186,151.787c0.203,4.501,0.305,9.023,0.305,13.565 c0,138.542-105.461,298.285-298.274,298.285c-59.209,0-114.322-17.357-160.716-47.104c8.212,0.973,16.546,1.47,25.012,1.47 c49.121,0,94.318-16.759,130.209-44.884c-45.887-0.841-84.596-31.154-97.938-72.804c6.408,1.227,12.968,1.886,19.73,1.886 c9.55,0,18.816-1.287,27.617-3.68c-47.955-9.633-84.1-52.001-84.1-102.795c0-0.446,0-0.882,0.011-1.318 c14.133,7.847,30.294,12.562,47.488,13.109c-28.134-18.796-46.637-50.885-46.637-87.262c0-19.212,5.16-37.218,14.193-52.7 c51.707,63.426,128.941,105.156,216.072,109.536c-1.784-7.675-2.718-15.674-2.718-23.896c0-57.891,46.941-104.832,104.832-104.832 c30.173,0,57.404,12.734,76.525,33.102c23.887-4.694,46.313-13.423,66.569-25.438c-7.827,24.485-24.434,45.025-46.089,58.002 c21.209-2.535,41.426-8.171,60.222-16.505C497.448,118.542,479.666,137.004,459.186,151.787z"> </path> </g> </g></svg>
+        <div class="filled" :style="{ backgroundColor: icon.backgroundColor }"></div>
+        <svg
+          :fill="icon.fill"
+          :version="icon.version"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          :viewBox="icon.viewBox"
+          xml:space="preserve"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <g v-html="icon.svgContent"></g>
+          </g>
+        </svg>
       </a>
-      <div class="tooltip">Twitter</div>
-    </li>
-    <li class="icon-content">
-      <a
-        href="javascript:void(0)"
-        aria-label="Facebook"
-        data-social="facebook"
-        @click="() => shareOnFacebook(index, userAnswerRefs)"
-      >
-        <div class="filled"></div>
-        <svg fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="7935ec95c421cee6d86eb22ecd11b7e3"> <path style="display: inline;" d="M283.122,122.174c0,5.24,0,22.319,0,46.583h83.424l-9.045,74.367h-74.379 c0,114.688,0,268.375,0,268.375h-98.726c0,0,0-151.653,0-268.375h-51.443v-74.367h51.443c0-29.492,0-50.463,0-56.302 c0-27.82-2.096-41.02,9.725-62.578C205.948,28.32,239.308-0.174,297.007,0.512c57.713,0.711,82.04,6.263,82.04,6.263 l-12.501,79.257c0,0-36.853-9.731-54.942-6.263C293.539,83.238,283.122,94.366,283.122,122.174z"> </path> </g> </g></svg>      </a>
-      <div class="tooltip">Facebook</div>
+      <div class="tooltip">{{ icon.ariaLabel }}</div>
     </li>
   </ul>
 </template>
 
-<script setup lang="ts">
-import { shareOnTwitter, shareOnFacebook } from '../services/shareService';
-import { ref } from 'vue';
-
-const userAnswerRefs = ref<HTMLElement[]>([]);
-const index = ref(0);
+<script>
+export default {
+  name: "SocialMedia",
+  props: {
+    icons: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+};
 </script>
+
+<style scoped>
+.example-2 .icon-content {
+  position: relative;
+  display: inline-block;
+  margin: 0 10px;
+}
+.example-2 .icon-content a {
+  display: block;
+  position: relative;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  border-radius: 50%;
+  overflow: hidden;
+  transition: all 0.3s ease-in-out;
+}
+.example-2 .icon-content a .filled {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 0;
+  transition: all 0.3s ease-in-out;
+}
+.example-2 .icon-content a:hover .filled {
+  height: 100%;
+}
+.example-2 .icon-content .tooltip {
+  position: absolute;
+  bottom: -25px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #000;
+  color: #fff;
+  padding: 5px;
+  border-radius: 3px;
+  font-size: 12px;
+  white-space: nowrap;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+}
+.example-2 .icon-content a:hover ~ .tooltip {
+  opacity: 1;
+}
+</style>
